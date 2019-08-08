345ae12769e54dd7f5e4467150265f8c6a3ef5ac
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-08-08 10:26:52 -0300

    add experimental pipeline

--- test_env/build.sh
+++ test_env/build.sh
@@ -1,8 +1,20 @@
-git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller
-docker build controller/ -t asperathos_controller:integration-tests
-git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor
-docker build monitor/ -t asperathos_monitor:integration-tests
-git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager
-docker build manager/ -t asperathos_manager:integration-tests
-git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer
-docker build visualizer/ -t asperathos_visualizer:integration-tests
+
+if [ $GIT_BRANCH == "experimental-*" ]; then
+    git clone https://github.com/ufcg-lsd/asperathos-controller --branch $GIT_BRANCH ./controller/asperathos-controller || git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller
+    docker build controller/ -t asperathos_controller:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-monitor --branch $GIT_BRANCH ./monitor/asperathos-monitor || git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor
+    docker build monitor/ -t asperathos_monitor:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-manager --branch $GIT_BRANCH ./manager/asperathos-manager || git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager
+    docker build manager/ -t asperathos_manager:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-visualizer --branch $GIT_BRANCH ./visualizer/asperathos-visualizer || git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer
+    docker build visualizer/ -t asperathos_visualizer:integration-tests
+else
+    git clone https://github.com/ufcg-lsd/asperathos-controller ./controller/asperathos-controller
+    docker build controller/ -t asperathos_controller:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-monitor ./monitor/asperathos-monitor
+    docker build monitor/ -t asperathos_monitor:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-manager ./manager/asperathos-manager
+    docker build manager/ -t asperathos_manager:integration-tests
+    git clone https://github.com/ufcg-lsd/asperathos-visualizer ./visualizer/asperathos-visualizer
+    docker build visualizer/ -t asperathos_visualizer:integration-tests
+fi
